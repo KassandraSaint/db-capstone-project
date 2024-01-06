2add61c51b92fb68b2b517400cdbf805b9096124
Kseniia Salko <64668217+KassandraSaint@users.noreply.github.com>
2024-01-05 21:33:33 -0500

    Added new stored procedures

--- Queries_StoredProcedures.sql
+++ Queries_StoredProcedures.sql
@@ -40,3 +40,94 @@ DELIMITER ;
 CALL CancelOrder(12);
 
 SELECT * FROM Orders;
+
+INSERT INTO Bookings
+VALUES
+(9, '2022-10-10', 5, 1, 2),
+(10, '2022-11-12', 3, 3, 1),
+(11, '2022-10-11', 2, 2, 3),
+(12, '2022-10-13', 2, 1, 5);
+
+DROP PROCEDURE IF EXISTS CheckBooking;
+
+DELIMITER //
+CREATE PROCEDURE CheckBooking(IN dt DATE, IN tble INT)
+BEGIN
+IF dt = (SELECT Date FROM Bookings WHERE Date = dt AND TableNumber = tble) THEN SELECT CONCAT('Table #', tble, ' is booked for ', dt) AS 'Booking Status';
+END IF;
+END //
+DELIMITER ;
+
+CALL CheckBooking('2022-10-10', 5);
+
+
+DROP PROCEDURE IF EXISTS AddValidBooking;
+
+DELIMITER //
+CREATE PROCEDURE AddValidBooking(dt DATE, tble INT)
+BEGIN
+DECLARE id INT;
+START TRANSACTION;
+SELECT (MAX(BookingID) + 1) FROM Bookings INTO id;
+
+IF dt = (SELECT Date FROM Bookings WHERE Date = dt AND TableNumber = tble) 
+THEN SELECT CONCAT('Table #', tble, ' is already booked for ', dt, ' - booking cancelled!') AS 'Booking Status';
+ROLLBACK;
+ELSE 
+INSERT INTO Bookings
+VALUES 
+(id, dt, tble, 1, 1);
+SELECT CONCAT('Table #', tble, ' was booked for ', dt) AS 'Booking Status';
+
+END IF;
+END //
+
+DELIMITER ;
+
+CALL AddValidBooking('2024-12-04', 2);
+
+
+DROP PROCEDURE IF EXISTS AddBooking;
+
+DELIMITER //
+
+CREATE PROCEDURE AddBooking(id INT, dt DATE, tble INT, c_id INT, s_id INT)
+BEGIN
+
+INSERT INTO Bookings
+VALUES 
+(id, dt, tble, c_id, s_id);
+SELECT CONCAT('New Booking # ', id , ' was addded') AS 'Confirmation';
+END //
+
+DELIMITER ;
+
+CALL AddBooking(9, '2022-10-10', 3, 4, 2);
+
+
+DROP PROCEDURE IF EXISTS UpdateBooking;
+
+DELIMITER //
+CREATE PROCEDURE UpdateBooking(id INT, dt DATE)
+BEGIN
+UPDATE Bookings SET Date = dt WHERE BookingID = id; 
+SELECT CONCAT('Booking # ', id , ' was updated') AS 'Confirmation';
+END //
+DELIMITER ;
+
+CALL UpdateBooking(9, '2022-12-17');
+
+
+DROP PROCEDURE IF EXISTS CancelBooking;
+
+DELIMITER //
+CREATE PROCEDURE CancelBooking(id INT)
+BEGIN
+DELETE FROM Bookings WHERE BookingID = id; 
+SELECT CONCAT('Booking #', id , ' cancelled') AS 'Confirmation';
+END //
+DELIMITER ;
+
+CALL CancelBooking(9);
+
+
